7ac50e5a344e6b1944126f0b2fa3c76591fe62cf
Rehoboth227 <67788089+Rehoboth227@users.noreply.github.com>
2020-07-03 19:10:28 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+function ask(question, yes, no, mul, div, err) {
+    let ops = prompt(question);
+ 
+     
+     console.log(ops);
+   if ( ops=== '+' ) {
+       let num1 = +prompt('Enter the number1')
+       let num2 = +prompt('Enter the number2')
+       yes(num1, num2);
+     }
+   else if(ops === '-') {
+       let num1 = +prompt('Enter the number1')
+       let num2 = +prompt('Enter the number2')
+       no(num1, num2);
+     }else if(ops === '*') {
+       let num1 = +prompt('Enter the number1')
+       let num2 = +prompt('Enter the number2')
+       mul(num1, num2);
+     }else if(ops === '/') {
+       let num1 = +prompt('Enter the number1')
+       let num2 = +prompt('Enter the number2')
+       div(num1, num2);
+     }
+   else {err()}
+ }
+ ask("Enter an operator ?", (a, b) => {alert( a + b );},  (a, b) => {alert( a - b );},  (a, b) => {alert( a * b );}, (a, b) => {alert( a / b );},() => { alert('try again later') });
