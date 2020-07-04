f04342cd9497ad413c584689cc5c933b11d75ddb
Obebe Joshua Damola <joshtrack800@gmail.com>
2020-07-04 13:56:13 +0100

    fix all bugs

--- index.js
+++ index.js
@@ -1,34 +1,67 @@
+// function ask(question, yes, no, mul, div, err) {
+//   let ops = ( yes, no, mul, div, err)
+//   if (ops === '+' || ops === '-' || ops ==='*' || ops ==='/'){
+//     console.log(ops);
+//   } else {
+//     return("try again later");
+//   }
+//   let num1 = +prompt('Enter the number1');
+//   let num2 = +prompt('Enter the number2');
+//    switch (ask){
+//      case '+':
+//       yes(num1, num2);
+//       break;
+//     case '-':
+//       no(num1, num2);
+//       break;
+//     case '*':
+//       mul(num1, num2);
+//       break;
+//     case '/':
+//       div(num1, num2);
+//       break;
+
+//     default:
+//       err();
+//    }   
+//   }     
+// ask("Enter an operator ?",
+// (a, b) => {alert( a + b );}, 
+//  (a, b) => {alert( a - b );}, 
+//   (a, b) => {alert( a * b );},
+//    (a, b) => {alert( a / b );},
+//    () => { alert('try again later') });
+
 function ask(question, yes, no, mul, div, err) {
-    let ops = prompt(question);
-    if (ops === '+' || ops === '-' || ops ==='*' || ops ==='/'){
-      console.log(ops);
-    } else {
-      return("try again later");
-    }
-    let num1 = +prompt('Enter the number1');
-    let num2 = +prompt('Enter the number2');
-     switch (ops){
-       case '+':
-        yes(num1, num2);
-        break;
-      case '-':
-        no(num1, num2);
-        break;
-      case '*':
-        mul(num1, num2);
-        break;
-      case '/':
-        div(num1, num2);
-        break;
+  let ops = prompt(question, '')
+  if (ops === '+' || ops === '-' || ops ==='*' || ops ==='/'){
+    console.log(ops);
+  } else {
+    return("try again later");
+  }
+  let num1 = +prompt('Enter the number1');
+  let num2 = +prompt('Enter the number2');
+   switch (ask){
+     case 'a + b':
+      yes(num1, num2);
+      break;
+    case 'a - b':
+      no(num1, num2);
+      break;
+    case 'a * b':
+      mul(num1, num2);
+      break;
+    case 'a / b':
+      div(num1, num2);
+      break;
 
-      default:
-        err();
-     }   
-    }     
- ask("Enter an operator ?",
-  (a, b) => {alert( a + b );}, 
-   (a, b) => {alert( a - b );}, 
-    (a, b) => {alert( a * b );},
-     (a, b) => {alert( a / b );},
-     () => { alert('try again later') });
-
+    default:
+      err();
+   }   
+  }     
+ask("Enter an operator ?",
+(a, b) => {alert( a + b );}, 
+ (a, b) => {alert( a - b );}, 
+  (a, b) => {alert( a * b );},
+   (a, b) => {alert( a / b );},
+   () => { alert('try again later') });
